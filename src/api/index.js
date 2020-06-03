67eeebb729cb1c70d61e50c6c822aa453e40217e
f-ke <1945334800@qq.com>
2020-06-04 03:24:04 +0800

    complete url

--- src/api/index.js
+++ src/api/index.js
@@ -4,13 +4,13 @@
 import ajax from './ajax'
 export const reqRegister = (user) =>ajax('https://job-seeking-server.herokuapp.com/register',user,'Post')
 
-export const reqLogin = ({username, password}) => ajax('/login',{username, password},'Post')
+export const reqLogin = ({username, password}) => ajax('https://job-seeking-server.herokuapp.com/login',{username, password},'Post')
 
 //update
-export const reqUpdateUser = (user) =>ajax('/update',user,'Post')
+export const reqUpdateUser = (user) =>ajax('https://job-seeking-server.herokuapp.com/update',user,'Post')
 
 
-export const reqUser =()=> ajax('/user')
-export const reqUserlist = (type)=>ajax('/userlist',{type})
-export const reqChatMsgLIST =()=>ajax('/msglist')
-export const reqReadMst =(from) =>ajax('/readmsg',{from},'Post')
+export const reqUser =()=> ajax('https://job-seeking-server.herokuapp.com/user')
+export const reqUserlist = (type)=>ajax('https://job-seeking-server.herokuapp.com/userlist',{type})
+export const reqChatMsgLIST =()=>ajax('https://job-seeking-server.herokuapp.com/msglist')
+export const reqReadMst =(from) =>ajax('https://job-seeking-server.herokuapp.com/readmsg',{from},'Post')
