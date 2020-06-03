c51b38b460fac722e3f7792114f0ba783f2cfc5e
f-ke <1945334800@qq.com>
2020-06-04 05:27:15 +0800

    Updates

--- precache-manifest.6e701e78db86ca5d873fcd8fbc15a874.js
+++ precache-manifest.6e701e78db86ca5d873fcd8fbc15a874.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0224b1ba1d895a5065e6b43b6d400235",
+    "revision": "62d51cd37c176124e73033566e01dd6f",
     "url": "/job-seeking-system-client/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/job-seeking-system-client/static/css/2.c1216e66.chunk.css"
   },
   {
-    "revision": "403f68dfd13c99916a10",
+    "revision": "14f64b1d599434b5aeca",
     "url": "/job-seeking-system-client/static/css/main.6014307b.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/job-seeking-system-client/static/js/2.085c08d9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "403f68dfd13c99916a10",
-    "url": "/job-seeking-system-client/static/js/main.eba521f2.chunk.js"
+    "revision": "14f64b1d599434b5aeca",
+    "url": "/job-seeking-system-client/static/js/main.ff103934.chunk.js"
   },
   {
     "revision": "c49c9b03e47731ecd8b2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/job-seeking-system-client/precache-manifest.a86dc46638cd905d8c2e70dbac01d48d.js"
+  "/job-seeking-system-client/precache-manifest.6e701e78db86ca5d873fcd8fbc15a874.js"
 );
 
 self.addEventListener('message', (event) => {
