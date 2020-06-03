b4febe48ebb89875837d1286047ef4999008f08a
f-ke <1945334800@qq.com>
2020-06-04 02:39:07 +0800

    Updates

---
+++ precache-manifest.39f9a26a84f5d8d6d7dfdd5413d0b6af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b9c59c78f68cd366a5cb791c7a135c",
+    "url": "/job-seeking-system-client/index.html"
+  },
+  {
+    "revision": "9b26090b20c6edf93a59",
+    "url": "/job-seeking-system-client/static/css/2.c1216e66.chunk.css"
+  },
+  {
+    "revision": "7fc56492dfe0e88e0320",
+    "url": "/job-seeking-system-client/static/css/main.6014307b.chunk.css"
+  },
+  {
+    "revision": "9b26090b20c6edf93a59",
+    "url": "/job-seeking-system-client/static/js/2.085c08d9.chunk.js"
+  },
+  {
+    "revision": "935d6192ca7feadfd09cd5b422cefa3f",
+    "url": "/job-seeking-system-client/static/js/2.085c08d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc56492dfe0e88e0320",
+    "url": "/job-seeking-system-client/static/js/main.8f2a49b8.chunk.js"
+  },
+  {
+    "revision": "c49c9b03e47731ecd8b2",
+    "url": "/job-seeking-system-client/static/js/runtime-main.159b1b18.js"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/job-seeking-system-client/static/media/logo.ba1f87ec.png"
+  }
+]);
