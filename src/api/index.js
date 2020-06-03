cf3a5533c0356f5574785aee27b5c0581310cdba
f-ke <1945334800@qq.com>
2020-06-04 04:22:31 +0800

    oh dear i am crazyy

--- src/api/index.js
+++ src/api/index.js
@@ -2,15 +2,15 @@
 
 //register
 import ajax from './ajax'
-export const reqRegister = (user) =>ajax('/register',user,'Post')
+export const reqRegister = (user) =>ajax('https://job-seeking-server.herokuapp.com/register',user,'Post')
 
 export const reqLogin = ({username, password}) => ajax('/login',{username, password},'Post')
 
 //update
-export const reqUpdateUser = (user) =>ajax('https://job-seeking-server.herokuapp.com/update',user,'Post')
+export const reqUpdateUser = (user) =>ajax('/update',user,'Post')
 
 
-export const reqUser =()=> ajax('https://job-seeking-server.herokuapp.com/user')
-export const reqUserlist = (type)=>ajax('https://job-seeking-server.herokuapp.com/userlist',{type})
-export const reqChatMsgLIST =()=>ajax('https://job-seeking-server.herokuapp.com/msglist')
-export const reqReadMst =(from) =>ajax('https://job-seeking-server.herokuapp.com/readmsg',{from},'Post')
+export const reqUser =()=> ajax('/user')
+export const reqUserlist = (type)=>ajax('/userlist',{type})
+export const reqChatMsgLIST =()=>ajax('/msglist')
+export const reqReadMst =(from) =>ajax('/readmsg',{from},'Post')
