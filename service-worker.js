97c51d068dc6c3707a2d9d7c2278f033ee38c7a8
f-ke <1945334800@qq.com>
2020-06-04 04:23:45 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/job-seeking-system-client/precache-manifest.39f9a26a84f5d8d6d7dfdd5413d0b6af.js"
+  "/job-seeking-system-client/precache-manifest.e841eef032ec5144f813b46ccb6abd71.js"
 );
 
 self.addEventListener('message', (event) => {
